d265e0abae50f312c777f08e0073b34f0c4938d2
Adarsh Rai <adarshrai100@gmail.com>
2026-02-21 04:36:51 +0530

    feat: implemented click-to-place placeholder tower system

--- Assets/Scripts/Grid/GridSelector.cs
+++ Assets/Scripts/Grid/GridSelector.cs
@@ -4,12 +4,14 @@ using UnityEngine.InputSystem;
 public class GridSelector : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
+    [SerializeField] private GameObject _towerPrefab;
 
     private GridCell _currentCell;
 
     private void Update()
     {
         HandleMouseHover();
+        HandlePlacement();
     }
 
     private void HandleMouseHover()
@@ -50,5 +52,28 @@ public class GridSelector : MonoBehaviour
             _currentCell.Highlight(false);
             _currentCell = null;
         }
+    }
+
+    private void HandlePlacement()
+    {
+        if (_currentCell == null) return;
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            if (!_currentCell.IsOccupied)
+            {
+                PlaceTower(_currentCell);
+            }
+        }
+    }
+
+    private void PlaceTower(GridCell cell)
+    {
+        Vector3 position = cell.transform.position;
+        position.y += 1f; // raise tower slightly above grid
+
+        Instantiate(_towerPrefab, position, Quaternion.identity);
+
+        cell.SetOccupied(true);
     }
 }
