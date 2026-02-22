f744c7129dbb89b7377fbe646af0aee72ed683f1
Adarsh Rai <adarshrai100@gmail.com>
2026-02-22 21:45:47 +0530

    feat: implemented progressive wave difficulty scaling

--- Assets/Scripts/Systems/WaveManager.cs
+++ Assets/Scripts/Systems/WaveManager.cs
@@ -3,15 +3,21 @@ using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
-    [Header("Wave Settings")]
-    [SerializeField] private EnemyMovement _enemyPrefab;
+    [Header("Core References")]
     [SerializeField] private PathManager _pathManager;
-    [SerializeField] private int _enemiesPerWave = 5;
-    [SerializeField] private float _spawnInterval = 1f;
-    [SerializeField] private float _timeBetweenWaves = 5f;
     [SerializeField] private BaseHealth _baseHealth;
     [SerializeField] private ObjectPool _enemyPool;
 
+    [Header("Wave Timing")]
+    [SerializeField] private float _spawnInterval = 1f;
+    [SerializeField] private float _timeBetweenWaves = 5f;
+
+    [Header("Wave Scaling")]
+    [SerializeField] private int _baseEnemiesPerWave = 5;
+    [SerializeField] private float _healthGrowthPerWave = 2f;
+    [SerializeField] private float _speedGrowthPerWave = 0.2f;
+
+    [Header("Base Enemy Stats")]
     [SerializeField] private float _basicHealth = 10f;
     [SerializeField] private float _basicSpeed = 3f;
 
@@ -22,7 +28,6 @@ public class WaveManager : MonoBehaviour
     [SerializeField] private float _tankSpeed = 1.5f;
 
     private int _currentWave = 0;
-
     public int CurrentWave => _currentWave;
 
     private void Start()
@@ -45,7 +50,9 @@ public class WaveManager : MonoBehaviour
 
     private IEnumerator SpawnWave()
     {
-        for (int i = 0; i < _enemiesPerWave; i++)
+        int enemiesThisWave = _baseEnemiesPerWave + (_currentWave - 1);
+
+        for (int i = 0; i < enemiesThisWave; i++)
         {
             SpawnEnemy();
             yield return new WaitForSeconds(_spawnInterval);
@@ -69,29 +76,49 @@ public class WaveManager : MonoBehaviour
 
     private EnemyType GetRandomEnemyType()
     {
-        int rand = Random.Range(0, 3);
-        return (EnemyType)rand;
+        float tankChance = Mathf.Clamp01(_currentWave * 0.05f);
+        float roll = Random.value;
+
+        if (roll < tankChance)
+            return EnemyType.Tank;
+
+        if (roll < 0.5f)
+            return EnemyType.Fast;
+
+        return EnemyType.Basic;
     }
 
     private void ConfigureEnemy(EnemyType type, EnemyMovement movement, EnemyHealth health)
     {
+        float scaledHealth;
+        float scaledSpeed;
+
         switch (type)
         {
             case EnemyType.Basic:
-                health.SetMaxHealth(_basicHealth);
-                movement.SetMoveSpeed(_basicSpeed);
+                scaledHealth = _basicHealth + (_currentWave * _healthGrowthPerWave);
+                scaledSpeed = _basicSpeed + (_currentWave * _speedGrowthPerWave);
+
+                health.SetMaxHealth(scaledHealth);
+                movement.SetMoveSpeed(scaledSpeed);
                 movement.SetColor(Color.white);
                 break;
 
             case EnemyType.Fast:
-                health.SetMaxHealth(_fastHealth);
-                movement.SetMoveSpeed(_fastSpeed);
+                scaledHealth = _fastHealth + (_currentWave * _healthGrowthPerWave);
+                scaledSpeed = _fastSpeed + (_currentWave * _speedGrowthPerWave);
+
+                health.SetMaxHealth(scaledHealth);
+                movement.SetMoveSpeed(scaledSpeed);
                 movement.SetColor(Color.yellow);
                 break;
 
             case EnemyType.Tank:
-                health.SetMaxHealth(_tankHealth);
-                movement.SetMoveSpeed(_tankSpeed);
+                scaledHealth = _tankHealth + (_currentWave * _healthGrowthPerWave);
+                scaledSpeed = _tankSpeed + (_currentWave * _speedGrowthPerWave);
+
+                health.SetMaxHealth(scaledHealth);
+                movement.SetMoveSpeed(scaledSpeed);
                 movement.SetColor(Color.red);
                 break;
         }
