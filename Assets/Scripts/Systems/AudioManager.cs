ba5e544caa3a884cfd8fa42c3aad5769d697f12f
Adarsh Rai <adarshrai100@gmail.com>
2026-02-26 01:52:08 +0530

    feat: added per-category volume control to audio manager

--- Assets/Scripts/Systems/AudioManager.cs
+++ Assets/Scripts/Systems/AudioManager.cs
@@ -11,6 +11,14 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioClip _sellClip;
     [SerializeField] private AudioClip _gameOverClip;
 
+    [SerializeField] private float _masterVolume = 1f;
+
+    [SerializeField] private float _shootVolume = 0.5f;
+    [SerializeField] private float _hitVolume = 0.4f;
+    [SerializeField] private float _upgradeVolume = 0.7f;
+    [SerializeField] private float _sellVolume = 0.7f;
+    [SerializeField] private float _gameOverVolume = 0.8f;
+
     private AudioSource _audioSource;
 
     private void Awake()
@@ -23,15 +31,15 @@ public class AudioManager : MonoBehaviour
         _audioSource = gameObject.AddComponent<AudioSource>();
     }
 
-    public void PlayShoot() => Play(_shootClip);
-    public void PlayHit() => Play(_hitClip);
-    public void PlayUpgrade() => Play(_upgradeClip);
-    public void PlaySell() => Play(_sellClip);
-    public void PlayGameOver() => Play(_gameOverClip);
+    public void PlayShoot() => Play(_shootClip, _shootVolume);
+    public void PlayHit() => Play(_hitClip, _hitVolume);
+    public void PlayUpgrade() => Play(_upgradeClip, _upgradeVolume);
+    public void PlaySell() => Play(_sellClip, _sellVolume);
+    public void PlayGameOver() => Play(_gameOverClip, _gameOverVolume);
 
-    private void Play(AudioClip clip)
+    private void Play(AudioClip clip, float volume)
     {
         if (clip != null)
-            _audioSource.PlayOneShot(clip);
+            _audioSource.PlayOneShot(clip, volume * _masterVolume);
     }
 }
